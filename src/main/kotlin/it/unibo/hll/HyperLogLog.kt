d92d0f7695deeb05185e2df2f141cca435c3eddd
Danilo Pianini <danilo.pianini@unibo.it>
2020-02-19 18:57:45 +0100

    Force non nullability of extension receiver, cleanup comments

--- src/main/kotlin/it/unibo/hll/HyperLogLog.kt
+++ src/main/kotlin/it/unibo/hll/HyperLogLog.kt
@@ -1,25 +1,18 @@
 package it.unibo.hll
 
-import com.google.common.cache.CacheBuilder
-import com.google.common.cache.CacheLoader
-import com.google.common.cache.LoadingCache
-import com.google.common.collect.ImmutableSet
+import it.unibo.alchemist.model.HarmonicCentrality.harmonicCentralityOf
 import it.unibo.alchemist.model.implementations.molecules.SimpleMolecule
 import it.unibo.alchemist.model.interfaces.Node
 import it.unibo.alchemist.protelis.AlchemistExecutionContext
 import net.agkn.hll.HLL
 import net.agkn.hll.HLLType
-import org.jgrapht.Graph
-import org.jgrapht.graph.SimpleGraph
 import org.protelis.lang.datatype.Field
-import org.protelis.vm.ExecutionContext
-import it.unibo.alchemist.model.HarmonicCentrality.harmonicCentralityOf
 
 @ExperimentalUnsignedTypes
 class HyperLogLog private constructor(val hll: HLL) {
 
     val cardinality by lazy { hll.cardinality() }
-    private val bytes by lazy {
+    private val bytes: ByteArray by lazy {
         hll.toBytes()
     }
     private val hashCode by lazy { bytes.contentHashCode() }
@@ -28,7 +21,6 @@ class HyperLogLog private constructor(val hll: HLL) {
         hll.type == HLLType.EMPTY -> other
         other.hll.type == HLLType.EMPTY -> this
         else -> HyperLogLog(hll.clone().apply { union(other.hll) })
-//        else -> cache.get(ImmutableSet.of(hll, other.hll))
     }
 
     override fun equals(other: Any?): Boolean {
@@ -42,14 +34,6 @@ class HyperLogLog private constructor(val hll: HLL) {
     companion object {
 
         @JvmField val EMPTY_HLL = HyperLogLog(emptyHLL())
-//        val cache = CacheBuilder.newBuilder()
-//            .maximumSize(100_000)
-//            .recordStats()
-//            .build(CacheLoader.from { origins: Set<HLL>? ->
-//                require(origins!!.size == 2)
-//                val ordered = origins.toList()
-//                HyperLogLog(ordered[0].clone().apply { union(ordered[1]) })
-//            })
 
         fun emptyHLL(log2m: Int = 11, regwidth: Int = 5) = HLL(log2m, regwidth)
         fun AlchemistExecutionContext<*>.intFromEnviroment(id: String, orElse: Int): Int =
@@ -92,27 +76,6 @@ object HarmonicCentrality {
                 it.setConcentration(hCMol, harmonicCentralityOf(it))
             }
         }
-//        val environment = context.getEnvironmentAccess()
-//        val graph: Graph<Node<Any>, *> = SimpleGraph(Any::class.java)
-//        environment.getNodes().asSequence()
-//            .onEach { graph.addVertex(it) }
-//            .map{ environment.getNeighborhood(it) }
-//            .map { neighborhood -> Pair(
-//                neighborhood.center,
-//                neighborhood.neighbors.asSequence()
-//                    // Only keep links from higher ids to lower ones.
-//                    // Also ensures that the vertex is added before each edge
-//                    .filter { it.id < neighborhood.center.id })
-//            }
-//            .flatMap { (center, neighbors) -> neighbors.map { center to it } }
-////            .onEach { (_, node) -> graph.addVertex(node) }
-//            .forEach { (n1, n2) -> graph.addEdge(n1, n2) }
-//        // Compute scores and inject in every node
-//        org.jgrapht.alg.scoring.HarmonicCentrality(graph, false, false)
-//            .scores
-//            .forEach { (node, value) ->
-//                node.setConcentration(SimpleMolecule("actualCentrality"), value)
-//            }
     }
 }
 
