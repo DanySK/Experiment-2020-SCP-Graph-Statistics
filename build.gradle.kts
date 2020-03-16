dd6e00ade9480777e8fcc283b3999dd48ef3eca6
Danilo Pianini <danilo.pianini@unibo.it>
2020-03-16 15:19:47 +0100

    Re-tune experiment size with new parameters

--- build.gradle.kts
+++ build.gradle.kts
@@ -52,7 +52,7 @@ if (System.getProperty("os.name").toLowerCase().contains("linux")) {
     14 * 1024L
 }
 val taskSizeFromProject: Int? by project
-val taskSize = taskSizeFromProject ?: 20 * 1024 // 5K nodes with 10 neighbors require a lot of memory
+val taskSize = taskSizeFromProject ?: 10 * 1024 // 5K nodes with 10 neighbors require a lot of memory
 val threadCount = maxOf(1, minOf(Runtime.getRuntime().availableProcessors(), heap.toInt() / taskSize ))
 
 val alchemistGroup = "Run Alchemist"
