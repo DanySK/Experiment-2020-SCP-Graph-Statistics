4809bed0e0510863a7f02e2e36f6d91254ac92d3
Danilo Pianini <danilo.pianini@unibo.it>
2020-03-18 14:00:04 +0100

    Add more memory margin for the converge experiment

--- build.gradle.kts
+++ build.gradle.kts
@@ -74,7 +74,7 @@ data class Experiment(
 )
 val customization = listOf(
     Experiment("simulation", (7.6 * 1024).toInt(), 0.5, arrayOf("speed", "meanNeighbors", "nodeCount")),
-    Experiment("converge", maxTaskSize = 2048, variables = arrayOf("diameter"))
+    Experiment("converge", maxTaskSize = 2048 + 512, variables = arrayOf("diameter"))
 ).groupBy { it.name }.mapValues { (_, list) -> list.first() }
 /*
  * Scan the folder with the simulation files, and create a task for each one of them.
